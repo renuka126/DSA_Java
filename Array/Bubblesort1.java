9432bc2eb49d47657cfddd17c6698369a8d9ca98
renuka123 <sunitapachghare@gmail.com>
2026-07-06 23:09:53 +0530

    Initial commit

---
+++ Array/Bubblesort1.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Bubblesort1 {
+    public static void main(String[] args) {
+        int[] my_array = {64, 34, 25, 12, 22, 11, 90, 5};
+
+        int n = my_array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (my_array[j] > my_array[j + 1]) {
+                    int temp = my_array[j];
+                    my_array[j] = my_array[j + 1];
+                    my_array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted array: ");
+        for (int num : my_array) {
+            System.out.print(num + " ");
+        }
+    }
+}
