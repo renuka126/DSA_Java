da8e9d5a4b0897ecbf899bbad76af95443bae951
renuka123 <sunitapachghare@gmail.com>
2026-07-12 23:25:30 +0530

    Acending order

---
+++ SelectionSort/SelectionSortInt.java
@@ -0,0 +1,27 @@
+package SelectionSort;
+public class SelectionSortInt {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        int comparisons = 0, swaps = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                comparisons++;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+                swaps++;
+            }
+        }
+
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
+    }
+}
