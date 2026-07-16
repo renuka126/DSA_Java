f59e94a003e8a7c3ca634d6f45bc6bd102e005f8
renuka123 <sunitapachghare@gmail.com>
2026-07-16 23:56:53 +0530

    Sorting

---
+++ SelectionSort/SelectionSort.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+         
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
