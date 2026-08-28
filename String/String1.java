16b09f81555b49a2a312c7bcd737d821662def0c
renuka123 <sunitapachghare@gmail.com>
2026-08-28 23:24:41 +0530

    String

---
+++ String/String1.java
@@ -0,0 +1,10 @@
+public class String1 {
+    public static void main(String[] args) {
+
+        String name = "Renuka";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("First character: " + name.charAt(0));
+    }
+}
